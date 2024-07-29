b4aee5de2f01a523656de2bab24d8c06d0dbfd46
syj <yejiseo0725@theboms.co.kr>
2024-07-29 17:16:58 +0900

    object

---
+++ app.js
@@ -0,0 +1,13 @@
+const player = {
+    name: "Troro",
+    points: 10,
+    skill: ["cook", "medical", "intellectual"],
+    genotype: "human",
+    fat: false,
+};
+
+console.log(player);
+console.log(player.name);
+console.log(player.genotype);
+console.log(player.points);
+console.log(player.fat);
