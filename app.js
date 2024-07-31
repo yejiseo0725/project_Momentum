fb02cac5930ccdf556ebf419e8e143d19ebcb2ff
syj <yejiseo0725@theboms.co.kr>
2024-07-31 17:15:51 +0900

    drinkable calculator

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const age = parseInt( prompt("How old are you?") );
 
-if (isNaN(age)) { // string === true
-    console.log("Please write a number!");
-} else { // integer === false
-    console.log("Thank you for writing your age. :3");
-};
+if (isNaN(age) || age < 0) { // string === true
+    console.log("Please write a real positive number!");
+} else if(age < 18) {
+    console.log("You are baby.");
+} else if (age >= 18 && age <= 50) {
+    console.log("You can drink! :D");
+} else if (age > 50 && age <= 80) {
+    console.log("You should exercise..");
+} else if (age > 80) {
+    console.log("You can do whatever you want.");
+}
