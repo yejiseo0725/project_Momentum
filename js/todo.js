520807a91c870d129e870cb3bcaf6eebc137261a
YEJI SEO <yejiseo0725@gmail.com>
2024-09-03 22:14:06 +0900

    todos

--- js/todo.js
+++ js/todo.js
@@ -1,9 +1,47 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = document.getElementById("todo-form input");
+const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo() {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paingToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
 
 function handleToDoSubmit(event) {
   event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  toDos.push(newTodo);
+  paingToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  console.log(parsedToDos);
+  parsedToDos.array.forEach((item) => console.log("this is the turn of", item));
+}
